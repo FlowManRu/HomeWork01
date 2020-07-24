837a8bdb32456f31ecde2a8dbb33b6ad000b2e80
FlowManRu <63112357+FlowManRu@users.noreply.github.com>
2020-07-25 00:19:01 +0300

    Home Work 01 Activity

---
+++ app/src/main/java/com/example/homework01/BadBoys.kt
@@ -0,0 +1,11 @@
+package com.example.homework01
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class BadBoys : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_bad_boys)
+    }
+}
